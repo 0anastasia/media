6bb604bc88f149730a5285b90688185c20f677ca
Anastasia <anastasia.chashch@mail.ru>
2023-09-01 20:05:24 +0500

    done

--- src/index.js
+++ src/index.js
@@ -6,6 +6,3 @@ import './js/MediaRecording';
 import './js/Positions';
 import './js/Timer';
 import './js/checkMediaRecording';
-
-// import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
-// import '../node_modules/@fortawesome/fontawesome-free/js/all';
